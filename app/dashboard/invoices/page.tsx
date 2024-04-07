6b8dcfb92c61b04292ff065cd843cbd9eed4c280
Darío Rumbo <dario.rumbo@gmail.com>
2024-04-07 15:06:33 -0300

    Search using searchParams

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,9 +1,16 @@
 import { lusitana } from "@/app/ui/fonts";
 import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import InvoicesTable from "@/app/ui/invoices/table";
 import Search from "@/app/ui/search";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
 
-export default function Page() {
-
+export default function Page({ searchParams } : { searchParams?: {
+    query?: string,
+    page?: string
+}}) {
+    const query = searchParams?.query || ''
+    const currentPage = Number(searchParams?.page) || 1
     return (
         <div className="w-full">
             <div className="flex w-full items-center justify-between">
@@ -13,6 +20,9 @@ export default function Page() {
                 <Search placeholder="Search invoices..." />
                 <CreateInvoice />
             </div>
+            <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+                <InvoicesTable query={query} currentPage={currentPage} />
+            </Suspense>
         </div>
     )
 }
